424b1c166b5a2e9183642810aaebaab8998f3223
monish <monishanand.j@gmail.com>
2024-01-08 10:41:42 +0530

    swap

---
+++ Arrays/src/swap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class swap {
+    public static void main(String[] args) {
+        int a[]={1,3,5,6,7};
+        swap(a,0,1);
+        System.out.println(Arrays.toString(a));
+
+    }
+    static void swap(int a[],int i,int j){
+        int t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+
+    }
+}
